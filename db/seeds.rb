e3a6139bd7a31d713cf9ea371bfdc81264bdc77c
felix-locate <felix@locate.ai>
2020-03-10 12:46:54 -0700

    Added seeds.rd

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Client.create(full_name: "Jason Bar", 
+    email: "jbar@jb.com", 
+    number_of_sites: 2, 
+    phase: "Active"
+)
+
+Client.create(full_name: "Jingle Smith", 
+    email: "jsmith@jingle.com", 
+    number_of_sites: 5, 
+    phase: "New"
+)
+
+Client.create(full_name: "Keanu", 
+    email: "keanu@breathtaking.com", 
+    number_of_sites: 9, 
+    phase: "Active"
+)
